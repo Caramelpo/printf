d00e6c4e25f1333f4fb0e67c51c25a3b94972996
Caramelpo <ikramelasri1234@gmail.com>
2023-05-27 14:56:21 -0700

    task 1

---
+++ 1-print_integer.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_integer - the function prints a num in base 10
+ * @list: integer printed in base 10
+ *
+ * Return: resultant length
+ **/
+int print_integer(va_list list)
+{
+	int s;
+	char *ptr_buffer;
+
+	p_buffer = itoa(va_arg(list, int), 10);
+
+	s = print((ptr_buffer != NULL) ? ptr_buffer : "NULL");
+
+	return (s);
+}
+
