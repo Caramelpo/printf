0dfbed8ed74eda4e719d74d8746457e93054cb59
AmalNadifi <amal.nadifi9@gmail.com>
2023-05-27 14:10:10 -0700

    Task2_finished

---
+++ 2-print_unsigned_int.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_unsigned - the function prints an unsigned int
+ * @list: the insgned int to print
+ * Return: the lenght of the unsginedint
+ */
+
+int print_unsigned(va_list list)
+{
+	int s;
+	char *ptr_buffer;
+
+	ptr_buffer = itoa(va_arg(list, unsigned int), 10);
+	s = print((ptr_buffer != NULL) ? ptr_buffer : "NULL");
+	return (s);
+}
